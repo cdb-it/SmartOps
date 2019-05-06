31fe64b0f96b714027b3031736404af8334737d9
thakship85 <50103550+thakship85@users.noreply.github.com>
2019-05-06 10:00:40 +0600

    Update page1.php

--- SmartOps/page1.php
+++ SmartOps/page1.php
@@ -1,29 +1,4 @@
 <?php
-echo "This is for the testing 123";
-test 10.58
-  TEST 11.35
 
-
-  test 11.39
-
-  TEST 11.50
-
-
-  TEST 11.39 
-  TEST 11.45
-
-  TEST 12.38 
-  TEST 12.45
-
-  TEST 12.36
-  TEST 1.15 PM
-  TEST 2.26 PM
-  TEST 2.27 PM 
-  TEST 8.16 AM- 2019/05/06
-  TEST 8.42 AM- 2016/05/06 
-TEST 8.46 AM- 2016/05/06
-  COMMIT 
-  THIS LATEST COMMIT 
-  commit test 9.08 am
 ?>
 
